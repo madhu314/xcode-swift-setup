1bd4307529c3b2b15c41468859ad26f66c7c8b84
Madhu <madhu@uncommon.is>
2017-05-12 13:26:17 +0530

    Project init

---
+++ Podfile
@@ -0,0 +1,7 @@
+
+target 'XcodeSetup' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  # Pods for help
+  pod 'SwiftLint'
+end
---
+++ XcodeSetup/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  XcodeSetup
+//
+//  Created by Madhusudhan Sambojhu on 12/05/17.
+//  Copyright © 2017 Uncommon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+}
